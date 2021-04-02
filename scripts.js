bedc6d330dd737386dfc20995e6fd872186e4e75
Bre Thunholm <bthunholm@gmail.com>
2021-04-01 15:14:34 -1000

    new solutions

--- scripts.js
+++ scripts.js
@@ -242,13 +242,10 @@ return result.reverse().join(' ')
 
 function capitalize(str) {
     const words = []
-    
     for (let word of str.split(' ')) {
        words.push(word[0].toUpperCase() + word.slice(1)) 
     }
-  
    return words.join(' ')
-    
     }
 
 
@@ -270,18 +267,155 @@ function capitalize(str) {
     }
 
 
+// Steps Question 
+
+
+function steps(n) {
+    for (let row = 0; row < n; row++) {
+       let stair = ''
+       for (let col = 0; col < n; col++) {
+           if(col <= row) {
+               stair += '#'
+           } else {
+               stair += ''
+           }
+       }
+        console.log(stair);
+    }
+}
+
+
+// Solution # 1
+
+function steps(n) {
+    for (let row = 0; row < n; row++) {
+        let stair = ''
+        for (let col = 0; n; col++) {
+            if(col <= row){
+                stair += '#'
+            } else {
+                stair += ' '
+            }
+            console.log(stair);
+        }
+        
+    }
+
+}
+
+// Solution #2 - Recursion 
+
+function steps(n, row = 0, stair = '') {
+    if(n === row) {
+        return 
+    }
+    if(n === stair.length) {
+        console.log(stair);
+       steps(n, row + 1)
+       return 
+    }
+
+    if(stair.length <= row) {
+        stair += '#'
+    } else {
+        stair += ' '
+    }
+    
+    steps(n, row, stair)
+
+}
+
+
+// Two sided pyramids 
+
+// Solution # 1
+
+function pyramid(n) {
+const midpoint = Math.floor((2*n-1)/2)
+
+for (let row = 0; row < n; row++) {
+    let level = ''
+
+    for (let col = 0; col < (2 * n -1); col++) {
+       if(midpoint - row <= col && midpoint + row >= col) {
+           level += '#';
+       } else {
+           level += ' '
+       }
+
+    }
+    console.log(level);
+}
+
+
+}
+
+// Solution # 2 Recursion 
+
+function pyramid(n, row = 0, level = '') {
+    if(row === n) {
+        return 
+    } 
+    
+    if(level.length === 2 * n -1) {
+        console.log(level);
+        return  pyramid(n, row + 1)
+    }
+
+    const midpoint = Math.floor((2 * n - 1) / 2)
+    let add;
+    if(midpoint - row <= level.length && midpoint + row >= level.length) {
+        add  = '#'
+    } else {
+        add = ' '
+    }
+    pyramid(n, row, level + add)
+    
+    }
+    
+
+
+// Find the vowels 
+
+
+// My solution 
+
+function vowels(str) {
+    const vowels = ['a','e','i','o','u']
+    let clean = str.replace(/[^A-Za-z']/g, "").toLowerCase().split('')
+    let count = 0;
+    for (const char of clean) {
+        if(vowels.includes(char)){
+            count += 1
+        } 
+    }
+
+    return count
+    
+    } 
 
 
 
+// Solution # 1
+
+function vowels(str) {
+   let count =0;
+   const checker = ['a','e','i','o','u']
+
+   for (let char of str.toLowerCase()) {
+       if (checker.includes(char)) {
+           count++
+       }
+   }
+   return count
+    } 
 
 
+// Solution 2
 
-
-
-
-
-
-
-
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi)
+  return matches ? matches.length : 0
+ } 
 
 
